fda66e43b3d7f8a47d3141f3645e14aaa635da74
Simeon Jenkins <55206+smuj@users.noreply.github.com>
2026-04-23 22:32:42 +0100

    Simplify cm5 asset code

--- src/heydsl/cm5_assets.py
+++ src/heydsl/cm5_assets.py
@@ -1,31 +1,48 @@
 from .asset import AssetType, ExternalAsset
 
+CM5_BASE_URL = "https://cdnjs.cloudflare.com/ajax/libs/codemirror"
+CM5_VERSION = "5.65.20"
+
+
+def make_cm5_asset(
+    asset_type: AssetType,
+    name: str,
+    integrity: str | None = None,
+    subdir: str | None = None,
+) -> ExternalAsset:
+    """Helper function to create a CodeMirror 5 asset."""
+    extension = "css" if asset_type == AssetType.STYLESHEET else "js"
+    url = "/".join(
+        [
+            CM5_BASE_URL,
+            CM5_VERSION,
+            f"{subdir}/" if subdir else "",
+            f"{name}.min.{extension}",
+        ]
+    )
+    return ExternalAsset(type=asset_type, url=url, integrity=integrity)
+
 
 def default_cm5_assets() -> list[ExternalAsset]:
     """Return the default CodeMirror 5 assets."""
-    assets = [
-        ExternalAsset(
-            type=AssetType.STYLESHEET,
-            url="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.20/codemirror.min.css",
-            integrity="sha512-uf06llspW44/LZpHzHT6qBOIVODjWtv4MxCricRxkzvopAlSWnTf6hpZTFxuuZcuNE9CBQhqE0Seu1CoRk84nQ==",
+    return [
+        make_cm5_asset(
+            AssetType.STYLESHEET,
+            "codemirror",
+            "sha512-uf06llspW44/LZpHzHT6qBOIVODjWtv4MxCricRxkzvopAlSWnTf6hpZTFxuuZcuNE9CBQhqE0Seu1CoRk84nQ==",
         ),
-        ExternalAsset(
-            type=AssetType.SCRIPT,
-            url="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.20/codemirror.min.js",
-            integrity="sha512-hJEMjfR4ds7mXbBot3i/R+CsiyYpeWSezXul8uLBT8wXAHPUdhRgl4LLTtMGwjarftGKiQqK1v20XScm0ANTPQ==",
+        make_cm5_asset(
+            AssetType.SCRIPT,
+            "codemirror",
+            "sha512-hJEMjfR4ds7mXbBot3i/R+CsiyYpeWSezXul8uLBT8wXAHPUdhRgl4LLTtMGwjarftGKiQqK1v20XScm0ANTPQ==",
         ),
     ]
-    return assets
 
 
 def curated_cm5_themes() -> dict[str, ExternalAsset]:
     """Return a curated list of popular CodeMirror 5 themes."""
     themes = {
-        name: ExternalAsset(
-            type=AssetType.STYLESHEET,
-            url=f"https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.20/theme/{name}.min.css",
-            integrity=integrity,
-        )
+        name: make_cm5_asset(AssetType.STYLESHEET, name, integrity, subdir="theme")
         for name, integrity in [
             (
                 "cobalt",
